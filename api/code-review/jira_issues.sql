6e0e7360e26831ee78982392d7d1fb3de6c199f0
ppolydoras <pavlos@raw-labs.com>
2024-11-12 23:47:46 +0200

    enhanced ira issue retrieval

---
+++ api/code-review/jira_issues.sql
@@ -0,0 +1,67 @@
+-- @param username (Optional) jira username
+-- @type username varchar
+-- @default username null
+-- @param jira_key (Optional) Jira Key
+-- @type jira_key varchar
+-- @default jira_key null
+-- @param jira_issue_creation_date (Optional) Creation date of Jira issue
+-- @type jira_issue_creation_date date
+-- @default jira_issue_creation_date null
+-- @param jira_project_key (Optional) Jira Project Key. If not specified, then issues from all projects are processed.
+-- @type jira_project_key varchar
+-- @default jira_project_key null
+-- @param github_repository_full_name GitHub repository name
+-- @type github_repository_full_name varchar
+-- @param is_without_pull_request if true then search for Jira issues without any related GitHub Pull Requests, otherwise search for Jira issues with GitHub Pull Request(s)
+-- @type is_without_pull_request boolean
+-- @default is_without_pull_request true
+-- @param is_jira_issue_open if true then search for open Jira issues, otherwise search for Closed, Done or Resolved Jira issues
+-- @type is_jira_issue_open boolean
+-- @default is_jira_issue_open true
+-- @param page Current page number.
+-- @type page integer
+-- @default page 1
+-- @param page_size Number of records per page.
+-- @type page_size integer
+-- @default page_size 25
+-- @return Retrieves Jira issues that may or may not have an associated open pull request.
+WITH prs AS (
+  SELECT
+    github_pull_request.number,
+    github_pull_request.title,
+    github_pull_request.created_at,
+    github_pull_request.updated_at,
+    github_pull_request.author,
+    github_pull_request.url,
+    REGEXP_MATCHES(github_pull_request.title, '([A-Z]+-[0-9]+)', 'g') AS issue_keys,
+    github_pull_request.assignees
+  FROM
+    github.github_pull_request
+  WHERE
+    upper(github_pull_request.state) = 'OPEN'
+    AND github_pull_request.repository_full_name=:github_repository_full_name
+),
+jira_no_pr AS (
+  SELECT
+    jira_issue."key" AS jira_issue_key,
+    jira_issue.summary AS jira_summary,
+    jira_issue.status AS jira_status,
+    jira_issue.assignee_display_name AS jira_assignee,
+    jira_issue.created AS jra_creation_date
+  FROM
+    jira.jira_issue
+  LEFT JOIN
+    prs ON jira_issue."key" = prs.issue_keys[1]
+  WHERE 
+    ((:is_jira_issue_open AND upper(jira_issue.status_category) NOT IN ('DONE', 'CLOSED', 'RESOLVED')) OR (upper(jira_issue.status_category) IN ('DONE', 'CLOSED', 'RESOLVED')))
+    AND ((:is_without_pull_request AND prs.issue_keys IS NULL) OR (not(:is_without_pull_request) AND prs.issue_keys IS NOT NULL))
+    AND (jira_issue.project_key = :jira_project_key OR :jira_project_key IS NULL)
+    AND ((:jira_issue_creation_date IS NOT NULL AND jira_issue.created>=GREATEST(current_date - interval '15' day, :jira_issue_creation_date::timestamp)) OR (:jira_issue_creation_date IS NULL AND jira_issue.created>= current_date - interval '1' month))
+    AND (jira_issue.assignee_display_name ilike concat('%', :username, '%') OR :username IS NULL)
+    AND (jira_issue."key" ilike :jira_key OR :jira_key IS NULL)
+
+)
+SELECT *
+FROM jira_no_pr
+ORDER BY jira_issue_key DESC
+LIMIT COALESCE(:page_size, 15) OFFSET (COALESCE(:page, 1) - 1) * COALESCE(:page_size, 15);
