27ec14074448d0cca3199c6faf6e1553e6cfaf9c
ppolydoras <pavlos@raw-labs.com>
2024-11-13 13:04:02 +0200

    optimized join condition

--- api/code-review/github-pull-requests.sql
+++ api/code-review/github-pull-requests.sql
@@ -33,7 +33,7 @@ WITH prs AS (
     github_pull_request.author -> 'avatar_url' as author_github_avatar_url, 
     github_pull_request.author -> 'login' as author_github_username,
     github_pull_request.author -> 'url' as author_github_user_url, 
-    REGEXP_MATCHES(github_pull_request.title, '([A-Z]+-[0-9]+)', 'g') AS issue_keys,
+   -- REGEXP_MATCHES(github_pull_request.title, '([A-Z]+-[0-9]+)', 'g') AS issue_keys,
     github_pull_request.assignees
   FROM
     github.github_pull_request 
@@ -65,7 +65,8 @@ jira_pr AS (
   FROM
     prs
   LEFT JOIN
-    jira.jira_issue ON jira_issue."key" = prs.issue_keys[1]
+    -- jira.jira_issue ON jira_issue."key" = prs.issue_keys[1]
+    prs ON prs.title ilike concat('%', jira_issue."key", '%') 
   WHERE
   (jira_issue.created>= (:pr_creation_date - interval '15' day)) -- consider Jira issues created 15 days before opening the respective Github Pull Request
   AND (jira_issue.assignee_display_name ilike concat('%', :username, '%') OR :username IS NULL)
